9e3634dac42cc150581c03bc338c5257e8529cff
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-09-07 13:54:45 +0530

    save

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,16 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const para= "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+  const [showPara, setPara]=useState(false);
+  const btnHandle=()=>{
+    setPara(true);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      
+      <button id="click" onClick={btnHandle}>Show Para</button>
+      {showPara && <p id="para">{para}</p>}
     </div>
   );
 }
